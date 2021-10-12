0badf8f1488201d23fa34931f4c70fa5bba2d542
Khsna <63132100+Ikhsanarya@users.noreply.github.com>
2021-10-12 23:41:43 +0700

    Problem 5

---
+++ Assets/Scripts/Command/InputHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Command
+{
+    public enum InputType
+    {
+        Keyboard,
+        Cursor
+    }
+    
+    public class InputHandler: MonoBehaviour
+    {
+        public CircleController circleController;
+        public InputType inputType;
+        
+        private Queue<Command> commands = new Queue<Command>();
+
+        private void FixedUpdate()
+        {
+            if (inputType == InputType.Keyboard)
+            {
+                Command moveCommand = HandleKeyboardMovement();
+                if (moveCommand != null)
+                {
+                    commands.Enqueue(moveCommand);
+                    moveCommand.Execute();
+                }
+            }
+            else if (inputType == InputType.Cursor)
+            {
+                Command moveCommand = HandleCursorMovement();
+                if (moveCommand != null)
+                {
+                    commands.Enqueue(moveCommand);
+                    moveCommand.Execute();
+                }
+            }
+
+        }
+
+        private Command HandleKeyboardMovement()
+        {
+            float inputX = Input.GetAxis("Horizontal");
+            float inputY = Input.GetAxis("Vertical");
+            
+            return new KeyboardMove(circleController, inputX, inputY);
+        }
+
+        private Command HandleCursorMovement()
+        {
+            Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            return new CursorMove(circleController, cursorPos);
+        }
+            
+    }
+}
